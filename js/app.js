f4e45bb8c0f1deaa46d9a89d92b7cfec0435937a
odinsubject09 <your-email@example.com>
2024-12-18 20:52:46 +0530

    added class app ,made meal and workout form active

--- js/app.js
+++ js/app.js
@@ -26,7 +26,28 @@ class CalorieTracker
     this._totalCalories-=workout.calories;
     this._render();
   }
-  
+  removeMeal(id)
+  {
+    const index=this._meals.findIndex(meal=>meal.id===id);
+    if(index!=-1)
+    {
+      const meal=this._meals[index];
+      this._totalCalories-=meal.calories;
+      this._meals.splice(index,1);
+      this._render();
+    }
+  }
+  removeWorkout(id)
+  {
+    const index=this._workouts.findIndex(workout=>workout.id===id);
+    if(index!=-1)
+    {
+      const workout=this._workouts[index];
+      this._totalCalories+=workout.calories;
+      this._workouts.splice(index,1);
+      this._render();
+    }
+  }
   _displayCaloriesProgress() {
     const progressEl = document.getElementById('calorie-progress');
     const percentage = (this._totalCalories / this._calorieLimit) * 100;
@@ -86,6 +107,52 @@ class CalorieTracker
     const totalCaloriesEl=document.getElementById('calories-total');
     totalCaloriesEl.innerHTML=this._totalCalories;
   }
+  _displayNewMeal(meal) {
+    const mealsEl = document.getElementById('meal-items');
+    const mealEl = document.createElement('div');
+    mealEl.classList.add('card', 'my-2');
+    mealEl.setAttribute('data-id', meal.id);
+    mealEl.innerHTML = `
+    <div class="card-body">
+    <div class="d-flex align-items-center justify-content-between">
+      <h4 class="mx-1">${meal.name}</h4>
+      <div
+        class="fs-1 bg-primary text-white text-center rounded-2 px-2 px-sm-5"
+      >
+        ${meal.calories}
+      </div>
+      <button class="delete btn btn-danger btn-sm mx-2">
+        <i class="fa-solid fa-xmark"></i>
+      </button>
+    </div>
+  </div>
+    `;
+
+    mealsEl.appendChild(mealEl);
+  }
+  _displayNewWorkout(workout) {
+    const workoutsEl = document.getElementById('workout-items');
+    const workoutEl = document.createElement('div');
+    workoutEl.classList.add('card', 'my-2');
+    workoutEl.setAttribute('data-id', workout.id);
+    workoutEl.innerHTML = `
+    <div class="card-body">
+    <div class="d-flex align-items-center justify-content-between">
+      <h4 class="mx-1">${workout.name}</h4>
+      <div
+        class="fs-1 bg-secondary text-white text-center rounded-2 px-2 px-sm-5"
+      >
+        ${workout.calories}
+      </div>
+      <button class="delete btn btn-danger btn-sm mx-2">
+        <i class="fa-solid fa-xmark"></i>
+      </button>
+    </div>
+  </div>
+    `;
+
+    workoutsEl.appendChild(workoutEl);
+  }
   _render()
   {
     this._displayCaloriesBurned();
@@ -113,11 +180,82 @@ class Workout
     this.id=Math.random.toString(16).slice(2);
   }
 }
-const tracker=new CalorieTracker();
+class App
+{
+  constructor()
+  {
+    this._tracker=new CalorieTracker();
+    document.getElementById('meal-form').addEventListener('submit',this._newItem.bind(this,'meal'));
+    document.getElementById('workout-form').addEventListener('submit',this._newItem.bind(this,'workout'));
+    document.getElementById('meal-items').addEventListener('click',this._removeItem.bind(this,'meal')); 
+    document.getElementById('workout-items').addEventListener('click',this._removeItem.bind(this,'workout'));
+    document
+      .getElementById('filter-meals')
+      .addEventListener('keyup', this._filterItems.bind(this, 'meal'));
 
-const breakfast=new Meal('breakfast',4000);
-tracker.addMeal(breakfast);
+    document
+      .getElementById('filter-workouts')
+      .addEventListener('keyup', this._filterItems.bind(this, 'workout'));
+  }
+  _newItem(type, e) {
+    e.preventDefault();
 
-const run=new Workout('Morning run',300);
-tracker.addWorkout(run);
+    const name = document.getElementById(`${type}-name`);
+    const calories = document.getElementById(`${type}-calories`);
+
+    // Validate inputs
+    if (name.value === '' || calories.value === '') {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    if (type === 'meal') {
+      const meal = new Meal(name.value, +calories.value);
+      this._tracker.addMeal(meal);
+      this._tracker._displayNewMeal(meal);
+    } else {
+      const workout = new Workout(name.value, +calories.value);
+      this._tracker.addWorkout(workout);
+      this._tracker._displayNewWorkout(workout);
+    }
+
+    name.value = '';
+    calories.value = '';
+
+    const collapseItem = document.getElementById(`collapse-${type}`);
+    const bsCollapse = new bootstrap.Collapse(collapseItem, {
+      toggle: true,
+    });
+  }
+  _removeItem(type, e) {
+    if (
+      e.target.classList.contains('delete') ||
+      e.target.classList.contains('fa-xmark')
+    ) {
+      if (confirm('Are you sure?')) {
+        const id = e.target.closest('.card').getAttribute('data-id');
+
+        type === 'meal'
+          ? this._tracker.removeMeal(id)
+          : this._tracker.removeWorkout(id);
+
+        e.target.closest('.card').remove();
+      }
+    }
+  }
+  
+  _filterItems(type, e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll(`#${type}-items .card`).forEach((item) => {
+      const name = item.firstElementChild.firstElementChild.textContent;
+
+      if (name.toLowerCase().indexOf(text) !== -1) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
+}
+const app=new App();
 
